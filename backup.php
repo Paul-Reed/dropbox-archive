2057f7567838c2c28f72a675902055c3a2eba78e
Paul-Reed <pauldreed@gmail.com>
2016-06-20 22:35:46 +0100

    Update backup.php to use bash script

--- backup.php
+++ backup.php
@@ -11,7 +11,6 @@
 
     */
 
-    require "config.php";
     include "lib/metadata.php";
     include "lib/mysql.php";
     include "lib/phptimeseries.php";
@@ -19,6 +18,18 @@
     include "lib/phpfiwa.php";
     include "lib/phpfina.php";
     include "lib/inputs.php";
+
+    // Import user settings
+    $fh=fopen("settings.conf", "r");
+    $pattern='/^(\w+)="([\w\/\.\-\:]+)"/';
+    while ($line=fgets($fh, 80)) {
+
+    if (preg_match($pattern, $line, $match)) {
+         $conf[$match[1]]=$match[2];
+         }
+    }
+    print_r($conf);
+    extract($conf);
 
     // Locate where script is installed
 
@@ -82,7 +93,7 @@
 
     // Fetch remote server feed list
 
-    $feeds = file_get_contents($emoncms_server."/feed/list.json?apikey=$emoncms_apikey");
+    $feeds = file_get_contents($emoncms_server."/feed/list.json?apikey=$emoncmsapikey");
     $feeds = json_decode($feeds);
 
     $number_of_feeds = count($feeds);
@@ -97,36 +108,29 @@
     {
 
         if ($feed->engine==0 && $mysqli) {
-            import_mysql($feed,$emoncms_server,$emoncms_apikey,$mysqli);
+            import_mysql($feed,$emoncms_server,$emoncmsapikey,$mysqli);
         }
 
         if ($feed->engine==1 && $feed->datatype==1) {
-            import_phptimestore($feed->id,$emoncms_server,$emoncms_apikey,$engines['phptimestore']['datadir']);
+            import_phptimestore($feed->id,$emoncms_server,$emoncmsapikey,$engines['phptimestore']['datadir']);
         }
 
         if ($feed->engine==2) {
-            import_phptimeseries($feed->id,$emoncms_server,$emoncms_apikey,$engines['phptimeseries']['datadir']);
+            import_phptimeseries($feed->id,$emoncms_server,$emoncmsapikey,$engines['phptimeseries']['datadir']);
         }
 
         if ($feed->engine==5) {
-            import_phpfina($feed->id,$emoncms_server,$emoncms_apikey,$engines['phpfina']['datadir']);
+            import_phpfina($feed->id,$emoncms_server,$emoncmsapikey,$engines['phpfina']['datadir']);
         }
 
         if ($feed->engine==6) {
-            import_phpfiwa($feed->id,$emoncms_server,$emoncms_apikey,$engines['phpfiwa']['datadir']);
+            import_phpfiwa($feed->id,$emoncms_server,$emoncmsapikey,$engines['phpfiwa']['datadir']);
         }
 
         if ($feed->engine==4 && $feed->datatype==1) {
-            import_phptimestore($feed->id,$emoncms_server,$emoncms_apikey,$engines['phptimestore']['datadir']);
+            import_phptimestore($feed->id,$emoncms_server,$emoncmsapikey,$engines['phptimestore']['datadir']);
         }
     }
-
-
-    // Dump MYSQL data
-
-    echo "Dumping MYSQL data\n";
-    $backup_file = $dbname . '_' . date("d-m-Y_His") . '.sql';
-    exec("mysqldump --lock-tables -u $dbuser -p$dbpass ". "$dbname > $tdir/mysql/$backup_file");
 
     // Backup nodered
 
@@ -145,62 +149,7 @@
       }
     }
 
-    // Create archive of temp_data in backups directory
-
-    try {
-    //make sure the script has enough time to run (600 seconds  = 10 minutes)
-    ini_set('max_execution_time', '600');
-    ini_set('set_time_limit', '0');
-    $target = isset($_GET["targetname"]) ? $_GET["targetname"] : 'backups/archive.tar';
-    $dir = isset($_GET["dir"]) ? $_GET["dir"] : "$tdir/."; //source is temp dir
-    //setup phar
-    $phar = new PharData($target);
-    $phar->buildFromDirectory(dirname(__FILE__) . '/'.$dir);
-    echo "Now compressing archive...\n";
-    file_put_contents("backups/archive_$date.tar.gz" , gzencode(file_get_contents('backups/archive.tar')));
-    unlink('backups/archive.tar');
-    } catch (Exception $e) {
-    // handle errors
-    echo 'An error has occured, details:';
-    echo $e->getMessage();
-    }
-
-    // Remove temporary directory and contents
-
-    $it = new RecursiveDirectoryIterator($tdir, RecursiveDirectoryIterator::SKIP_DOTS);
-    $files = new RecursiveIteratorIterator($it,
-             RecursiveIteratorIterator::CHILD_FIRST);
-    foreach($files as $file) {
-    if ($file->isDir()){
-        rmdir($file->getRealPath());
-    } else {
-        unlink($file->getRealPath());
-      }
-    }
-    rmdir($tdir);
-
-    // Remove archives older than x days from backups directory
-
-    if (file_exists("backups")) {
-    echo "\n..Checking for expired archives...\n";
-    foreach (new DirectoryIterator("backups") as $fileInfo) {
-        if ($fileInfo->isDot()) {
-        continue;
-        }
-
-        if (time() - $fileInfo->getCTime() >= ("$store"*60*60*24)-(60*60)) {
-           echo "Deleting expired archive - $fileInfo\n";
-            unlink($fileInfo->getRealPath());
-        }
-      }
-    }
-
-    // Delete cloud expired archives
+    // Run bash script
 
     $output = exec("lib/./sync.sh");
-
-    // Upload archive to Dropbox
-
-    echo "Uploading new archive to Dropbox, this may take a while...\n";
-    $output = exec("lib/./dropbox_uploader.sh -sf /home/pi/.dropbox_uploader upload backups/ /");
 
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-eval `egrep '^([a-zA-Z_]+)="(.*)"' settings.conf`
-
-echo "$dbuser"
-echo "$emoncms_server"
-echo "$NRdir"
-echo "$store"
-
